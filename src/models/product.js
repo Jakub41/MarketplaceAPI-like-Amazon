101e4e11a6e7c20a0dfdd2188977906afbac89f3
jakub41 <lemiszewski@gmx.com>
2019-12-13 14:28:08 +0100

    Implemented the model for create a Product

---
+++ src/models/product.js
@@ -0,0 +1,45 @@
+// The data file JSON
+const filename = "../db/products.json";
+
+// We assign the data
+let products = require(filename);
+
+// We import the helpers as wqe need to interact with our data
+const helper = require("../helpers/helper");
+
+// GET All Products
+const getAllProducts = () => {};
+
+// GET One Product
+const getOneProduct = id => {};
+
+// POST Create the Product
+const createProduct = newProduct => {
+    return new Promise((resolve, reject) => {
+        const id = { id: helper.getNewId() };
+        const date = {
+            created_at: helper.newDate(),
+            updated_at: helper.newDate()
+        };
+
+        newProduct = { ...id, ...date, ...newProduct };
+        products.push(newProduct);
+        helper.writeJSONFile(filename, products);
+        resolve(newProduct).catch(err => reject(err));
+    });
+};
+
+// PUT Update the Product
+const updateProduct = (id, newProduct) => {};
+
+// Delete Product
+const deleteProduct = id => {};
+
+// Exporting the modules
+module.exports = {
+    getAllProducts,
+    getOneProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+};
