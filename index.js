08c6bdc123185d9d0f00eec68f88d06cf69d2b97
jakub41 <lemiszewski@gmx.com>
2019-12-13 12:06:26 +0100

    Added validation middleware fro products

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 // Env variables from config.js
-const  { port } = require("./src/config/config")
+const { port } = require("./src/config/config");
 
 // Express lib
-const express = require("express")
+const express = require("express");
+
+// Body parser lib
+const bodyParser = require("body-parser");
 
 // CORS lib
-const cors = require("cors")
+const cors = require("cors");
+
+// Express validator
+//const expressValidator = require("express-validator");
 
 // Morgan lib logger middleware https://www.npmjs.com/package/morgan
 // To log app activities a good way to keep track of what is going on
 // Useful also to the debug issues when an exception come ups
-const morgan = require("morgan")
+const morgan = require("morgan");
 
 // Defining the server/app
-const server = express()
+const server = express();
 
 // Using CORS
-server.use(cors())
+server.use(cors());
 
 // We use Morgan to log our server
 // "tiny" The minimal output of the log the default light param
-server.use(morgan("tiny"))
+server.use(morgan("tiny"));
+
+// Using the Body parser lib
+server.use(bodyParser.json());
+
+// Using Express validator
+// server.use(expressValidator());
 
 // Starting the server on env port
 server.listen(`${port}`, () => {
     // Showing a message to the console informing on which port is running
     console.log(`Server is running on port ${port}`);
-})
+});
---
+++ src/middlewares/validatorRules.middleware.js
@@ -0,0 +1,19 @@
+import { matchedData, validationResult } from 'express-validator';
+
+const validateRules = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        req.matchedData = matchedData(req);
+        return next();
+    }
+
+    const extractedErrors = [];
+    errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+    return res.status(422).json({
+        errors: extractedErrors,
+    });
+}
+
+export default validateRules;
