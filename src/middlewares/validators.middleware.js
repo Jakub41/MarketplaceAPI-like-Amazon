8e1ce5cfe9f76aead9b1d4ff5b4ac5a12a44f5f2
jakub41 <lemiszewski@gmx.com>
2019-12-19 14:10:01 +0100

    Fixed a validation issue regarding a bug with empty strings

--- src/middlewares/validators.middleware.js
+++ src/middlewares/validators.middleware.js
@@ -6,24 +6,31 @@ const productValidationRulesPOST = [
     // Name must be min 3 characters and required
     body("name", "Name is required and 3 characters at least")
         .exists()
+        .notEmpty()
         .isLength({ min: 3 }),
     // Description must be min 10 characters and required
     body("description", "Description is required and 10 characters at least")
         .exists()
+        .notEmpty()
         .isLength({ min: 10 }),
     // Brand required and min length 3
     body("brand", "3 characters at least")
         .exists()
+        .notEmpty()
         .isLength({ min: 3 }),
     // Image URL not required
-    body("imageUrl").optional(),
+    body("imageUrl")
+        .optional()
+        .notEmpty(),
     // Price as a number and required
     body("price", "Price is required")
         .exists()
+        .notEmpty()
         .isNumeric(),
     // Category not required min length 3
     body("category", "3 characters at least")
         .optional()
+        .notEmpty()
         .isLength({ min: 3 })
 ];
 
@@ -34,6 +41,7 @@ const productUpdateRules = [
             // Name must be min 3 characters and required
             body("name", "Name is required and 3 characters at least")
                 .exists()
+                .notEmpty()
                 .isLength({ min: 3 }),
             // Description must be min 10 characters and required
             body(
@@ -41,20 +49,26 @@ const productUpdateRules = [
                 "Description is required and 10 characters at least"
             )
                 .exists()
+                .notEmpty()
                 .isLength({ min: 10 }),
             // Brand required and min length 3
             body("brand", "3 characters at least")
                 .exists()
+                .notEmpty()
                 .isLength({ min: 3 }),
             // Image URL not required
-            body("imageUrl").optional(),
+            body("imageUrl")
+                .notEmpty()
+                .optional(),
             // Price as a number and required
             body("price", "Price is required")
                 .exists()
+                .notEmpty()
                 .isNumeric(),
             // Category not required min length 3
             body("category", "3 characters at least")
                 .optional()
+                .notEmpty()
                 .isLength({ min: 3 })
         ]
     )
@@ -63,5 +77,5 @@ const productUpdateRules = [
 // Exports the required methods
 module.exports = {
     productValidationRulesPOST,
-    productUpdateRules,
+    productUpdateRules
 };
